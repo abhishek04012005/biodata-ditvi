c20ab905529d082e8057c041e35f6f46d5f87fa7
abhishek04012005 <abhishekchoudhary5414@gmail.com>
2026-05-06 16:56:45 +0530

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -7,6 +9,27 @@ const __dirname = dirname(__filename);
 const eslintConfig = [
   {
     ignores: [".next/**", "node_modules/**", ".git/**"],
+  },
+  {
+    files: ["**/*.js", "**/*.jsx"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parser: tsParser,
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+    },
   },
 ];
 
